d379fca35813272cbca8a7489f3fe203743d54fc
Rosa <rocanawt@gmail.com>
2022-12-02 15:47:35 -0500

    Rolling back repo

--- src/redux/actions/expenseActions.js
+++
@@ -1,8 +0,0 @@
-import {ActionTypes} from '../constants/expenseTypes';
-
-export function addExpense(item) {
-  return {
-    type: ActionTypes.ADD_EXPENSE,
-    data: item,
-  };
-}
--- src/screens/Tabs/HistoryScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native';
-
-const HistoryScreen = props => {
-  return <Text>History</Text>;
-};
-
-export default HistoryScreen;
